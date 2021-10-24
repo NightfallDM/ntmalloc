e760eefaa7777fb34aff265a81356083d74c5b92
night <night@halfware.top>
2021-10-24 17:27:15 +0800

    early stage: sync

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use libc;
+
 fn main() {
+    unsafe{
+        let cur_brk = libc::sbrk(0);
+        println!("cur_brk = 0x{:<16x}", cur_brk as usize);
+        let addr = cur_brk as usize + (1 << 12);
+        println!("addr = 0x{:<16x}", addr);
+        let ret = libc::brk(addr as *mut libc::c_void);
+        if let 0 = ret{
+            println!("modify break finish");
+            println!("cur_brk = 0x{:<16x}", libc::sbrk(0) as usize);
+        }else {
+            println!("ret = {}", ret as i32);
+        }
+    }
     println!("Hello, world!");
 }
--- src/ntmalloc.rs
+++ src/ntmalloc.rs
@@ -1,7 +1,49 @@
 use std::alloc::{GlobalAlloc, Layout};
-use libc;
+use errno::Errno;
+pub use libc;
+
+// provide base posix memory manager interface
+// also with rust style
+pub trait PosixMemRust{
+    pub fn malloc(&self, size: usize) -> Result<*mut u8, Errno>;
+    pub fn calloc(&self, nelem: usize, elesize: usize) -> Result<*mut u8, Errno>;
+    pub fn realloc(&self, addr: *mut u8, size: usize) -> Result<*mut u8, Errno>;
+    pub fn reallocarray(&self, addr *mut u8, nelem: usize, elesize: usize) -> Result<*mut u8, Errno>;
+    pub fn free(&self, addr: *mut u8);
+}
 
 pub struct Ntmalloc;
+
+impl PosixMenRust for Ntmalloc {
+    pub fn malloc(&self, size: usize) -> Result<*mut u8, Errno> {
+        // FIXME: use origin Linux syscall instead C Library to implement
+        // See the diff of "brk" between Linux/C library
+        let cur_brk = libc::sbrk(0);
+        if (cur_brk as i32 == -1){
+            println!("call sbrk failed");
+            return Err(Errno::last());
+        }
+
+    }
+}
+
+pub enum BaseAllocRegionFg {
+    
+}
+
+
+pub struct BaseAllocRegion {
+    base:   u64,
+    size:   u64,
+    flag:   BaseAllocRegionFg,
+}
+
+const static BASE_REGION_NUMS = 128;
+static mut BASE_REGIONS: [BaseAllocRegion; BASE_REGION_NUMS];
+pub struct BaseAlloc {
+
+}
+
 
 unsafe impl GlobalAlloc for Ntmalloc{
     unsafe fn alloc(&self, layout: Layout) -> *mut u8{
